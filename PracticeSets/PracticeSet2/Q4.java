9081d802b09166ee804e0f54dbffe349ae6fa65a
DarshanBothra <hitmandarshan10@gmail.com>
2025-05-19 23:14:03 +0530

    Completed Practice Set 2 - Problem 4

---
+++ PracticeSets/PracticeSet2/Q4.java
@@ -0,0 +1,12 @@
+public class Q4{
+    public static void main(String args[]){
+        float v, u, a, s;
+        v = 2.2f;
+        u = 2.0f;
+        a = 1.0f;
+        s = 10f;
+        float result =( v*v -u*u)/(2*a*s);
+
+        System.out.println(result);
+    }
+}
