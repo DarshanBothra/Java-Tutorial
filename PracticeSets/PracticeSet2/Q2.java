db3cd2b0a82c2c218e7093b818b7f06d010e0f3b
DarshanBothra <hitmandarshan10@gmail.com>
2025-05-19 23:07:34 +0530

    Completed Practice Set 2 - Problem 2

---
+++ PracticeSets/PracticeSet2/Q2.java
@@ -0,0 +1,8 @@
+public class Q2{
+    public static void main(String args[]){
+        char grade = 'A';
+        char encrypted_grade = (char)(grade + 8);
+        System.out.printf("Encrypted Grade: %c\n", encrypted_grade);
+        System.out.printf("Grade: %c\n", grade);
+    }
+}
