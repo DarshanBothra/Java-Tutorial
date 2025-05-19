699fbdf7b21e806389962e31afe1d577fc938b13
DarshanBothra <hitmandarshan10@gmail.com>
2025-05-19 18:36:29 +0530

    Completed Problem 1

---
+++ PracticeSets/PracticeSet1/Q1.java
@@ -0,0 +1,13 @@
+public class Q1{
+    public static void main(String args[]){
+        // Add 3 numbers and display the sum;
+        int num1, num2, num3, sum;
+        num1 = 10;
+        num2 = 5;
+        num3 = 12;
+
+        sum = num1 + num2 + num3;
+
+        System.out.printf("The sum of %d, %d, %d is: %d\n", num1, num2, num3, sum);
+    }
+}
