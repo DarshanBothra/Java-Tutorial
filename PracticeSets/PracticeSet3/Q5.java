cacd6120814cb6c2e41060074b07ada026e09d87
DarshanBothra <hitmandarshan10@gmail.com>
2025-05-21 12:30:05 +0530

    Practice Set 3 - Question 5

---
+++ PracticeSets/PracticeSet3/Q5.java
@@ -0,0 +1,8 @@
+public class Q5{
+    public static void main(String args[]){
+        String template = "Hey Darshan, I hope you are doing great. Regards, Self\n";
+        String updated = "Hey Darshan,\n\t I hope you are doing great.\nRegards,\nSelf";
+        System.out.println(template);
+        System.out.println(updated);
+    }
+}
