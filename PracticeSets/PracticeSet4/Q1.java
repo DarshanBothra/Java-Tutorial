2cb474e291c888cb27b7230ff9eafcb31f649ea0
DarshanBothra <hitmandarshan10@gmail.com>
2025-05-21 13:34:11 +0530

    Practice Set 4 - Question 1

---
+++ PracticeSets/PracticeSet4/Q1.java
@@ -0,0 +1,12 @@
+public class Q1{
+    public static void main(String args[]){
+        int a = 10;
+        if (a = 11){
+            System.out.println("I am 11");
+        }
+        else{
+            System.out.println("I am not 11");
+        }
+    }
+}
+// Output - Code generates error! at line 4 ('==': comparison != '=': assignment)
