2e9abf78836b36483ccffaa299b055556e2c8dd0
DarshanBothra <hitmandarshan10@gmail.com>
2025-05-21 12:29:48 +0530

    Practice Set 3 - Question 4

---
+++ PracticeSets/PracticeSet3/Q4.java
@@ -0,0 +1,7 @@
+public class Q4{
+    public static void main(String args[]){
+        String st = "Hey Darshan,  How are you?   ";
+        System.out.printf("Double Space at: %d\n", st.indexOf("  "));
+        System.out.printf("Triple Space at: %d\n", st.indexOf("   "));
+    }
+}
